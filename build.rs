90fc2f8a91480b2289b4ab4a86b59ddcc6088700
Ryan Russell <russellsprouts2@gmail.com>
2018-03-25 11:10:16 -0700

    Added hack to get correct lifetimes for grammar

--- build.rs
+++ build.rs
@@ -1,5 +1,44 @@
 extern crate peg;
+extern crate regex;
+
+use regex::Regex;
+use std::io;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
+use std::convert::AsRef;
+use std::fs::File;
+use std::process::exit;
+use std::env;
 
 fn main() {
-    peg::cargo_build("grammar.rustpeg");
+    let mut stderr = io::stderr();
+    let input_path: &Path = "grammar.rustpeg".as_ref();
+
+    let mut peg_source = String::new();
+    if let Err(e) = File::open(input_path).and_then(|mut x| x.read_to_string(&mut peg_source)) {
+        writeln!(stderr, "Could not read PEG input file `{}`: {}", input_path.display(), e).unwrap();
+        exit(1);
+    }
+
+    println!("cargo:rerun-if-changed={}", input_path.display());
+
+    let result = peg::compile(&peg_source);
+
+    let re = Regex::new(r"fn ([_a-zA-Z0-9]*?) < 'input >").unwrap();
+
+    let original_rust_source = match result {
+        Ok(s) => s,
+        Err(..) => {
+            writeln!(stderr, "Error compiling PEG grammar").unwrap();
+            exit(1);
+        }
+    };
+
+    let rust_source = re.replace_all(&original_rust_source, "fn $1 < 'input, 'a >");
+
+    let out_dir: PathBuf = env::var_os("OUT_DIR").unwrap().into();
+    let rust_path = out_dir.join(input_path.file_name().unwrap()).with_extension("rs");
+
+    let mut output_file = File::create(&rust_path).unwrap();
+    output_file.write_all(rust_source.as_bytes()).unwrap();
 }
--- main.rs
+++ main.rs
@@ -1,47 +1,171 @@
-extern crate regex;
-use regex::Regex;
-use std::env;
+#![feature(nll)]
+#![feature(underscore_lifetimes)]
+
+extern crate core;
+extern crate typed_arena;
 use std::fs::File;
 use std::io::prelude::*;
 use std::vec::Vec;
-use std::str::Chars;
-use std::iter::Peekable;
-use std::rc::Rc;
+use std::hash::Hash;
+use std::hash::Hasher;
+use std::collections::HashMap;
+use typed_arena::Arena;
 
-mod Grammar {
+mod grammar {
     include!(concat!(env!("OUT_DIR"), "/grammar.rs"));
 }
 
 #[derive(Debug)]
-pub enum Expression {
-    Integer(i64)
-}
-
-pub enum Type {
-    
+pub struct Arg<'a> {
+    value : &'a Expression<'a>,
+    name : Option<String>
 }
 
 #[derive(Debug)]
-pub enum AstNode {
+pub struct FnArg {
+    name: String,
+    declared_type: Type
+}
+
+#[derive(Debug)]
+pub enum UnaryOp {
+    Minus, LogicalNot, BitwiseNot, Indirect, AddressOf
+}
+
+#[derive(Debug)]
+pub enum BinaryOp {
+    LogicalOr, LogicalAnd, BitwiseOr, BitwiseXor, BitwiseAnd, Equal, NotEqual, LT, LE, GT, GE, LShift, RShift, Plus, Minus, Times, Divide, Mod
+}
+
+#[derive(Debug)]
+pub enum Expression<'a> {
+    Integer { 
+        value: i64,
+        loc : usize
+    },
+    Identifier {
+        name: String,
+        loc: usize
+    },
+    Call {
+        callee: &'a Expression<'a>,
+        args: Vec<Arg<'a>>,
+        loc: usize
+    },
+    Dot {
+        expression: &'a Expression<'a>,
+        property: String,
+        loc: usize
+    },
+    Subscript {
+        array: &'a Expression<'a>,
+        index: &'a Expression<'a>,
+        loc: usize
+    },
+    UnaryOp {
+        expression: &'a Expression<'a>,
+        op: UnaryOp,
+        loc: usize
+    },
+    BinaryOp {
+        a: &'a Expression<'a>,
+        op: BinaryOp,
+        b: &'a Expression<'a>,
+        loc: usize
+    },
+    Cast {
+        expression: &'a Expression<'a>,
+        new_type: Type,
+        loc: usize
+    }
+}
+
+#[derive(Debug)]
+pub struct TypeArg {
+    value : Type,
+    name : Option<String>
+}
+
+#[derive(Debug)]
+pub enum Type {
+    Identifier(String),
+    GenericType {
+        name: String,
+        args: Vec<TypeArg>
+    },
+    AnonymousStruct(Vec<(String, Type)>)
+}
+
+#[derive(Debug)]
+pub struct OptionValue<'a> {
+    name: Option<String>,
+    value: &'a Expression<'a>
+}
+
+pub struct Ast<'a> {
+    node_factory: Arena<AstNode<'a>>,
+    expr_factory: Arena<Expression<'a>>,
+}
+
+impl<'a> Ast<'a> {
+    fn new<'b>() -> Ast<'b> {
+        Ast {
+            node_factory: Arena::new(),
+            expr_factory: Arena::new()
+        }
+    }
+
+    fn parse(ast: &'a Ast<'a>, str: &String) -> Vec<&'a AstNode<'a>> {
+        grammar::program(str, ast).unwrap()
+    }
+    
+    fn create(&'a self, node: AstNode<'a>) -> &'a AstNode {
+        self.node_factory.alloc(node)
+    }
+
+    fn create_exp(&'a self, exp: Expression<'a>) -> &'a Expression {
+        self.expr_factory.alloc(exp)
+    }
+}
+
+#[derive(Debug)]
+pub enum AstNode<'a> {
     Block {
-        statements : Vec<Rc<AstNode>>
+        statements : Vec<&'a AstNode<'a>>
     },
     If {
-      condition : Rc<Expression>,
-      statement : Rc<AstNode>
+      condition : &'a Expression<'a>,
+      consequent : &'a AstNode<'a>,
+      alternate : Option<&'a AstNode<'a>>
     },
     FnDef {
         name: String,
-        returnType: Type
+        args: Vec<FnArg>,
+        return_type: Type,
+        body: &'a AstNode<'a>
+    },
+    Return {
+        expression: Option<&'a Expression<'a>>
+    },
+    Expression {
+        expression: &'a Expression<'a>
+    },
+    VarDefinition {
+        name: String,
+        declared_type: Option<Type>,
+        initializer: Option<&'a Expression<'a>>
+    },
+    OptionBlock {
+        options: Vec<&'a OptionValue<'a>>,
+        statement: &'a AstNode<'a>
+    },
+    StructDefinition {
+        name: String,
+        fields: Vec<(String, Type)>
     }
 }
 
 fn main() {
-    match Grammar::fn_definition("fn a() -> i8") {
-        Ok(r) => println!("Parsed as: {:?}", r),
-        Err(e) => println!("Parse error: {}", e)
-    }
-
     let filename = "test.65";
 
     println!("In file {}", filename);
@@ -53,4 +177,92 @@ fn main() {
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+
+    let ast = Box::new(Ast::new());
+    let parsed = Ast::parse(&ast, &contents);
+    println!("Parsed as\n{:#?}", parsed);
+}
+
+trait HasId {
+    fn id(&self) -> u64;
+}
+
+struct HashById<T>(T);
+
+impl<T> Hash for HashById<T> where T : HasId {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        match self {
+            &HashById(ref inner) => inner.id().hash(state)
+        }
+    }
+}
+
+trait Children<'a, T> {
+    fn children(&'a self) -> Vec<&'a T>;
+}
+
+impl<'a> Children<'a, AstNode<'a>> for Vec<&'a AstNode<'a>> {
+    fn children(&'a self) -> Vec<&'a AstNode<'a>> {
+        self.clone()
+    }
+}
+
+impl<'a> Children<'a, AstNode<'a>> for AstNode<'a> {
+    fn children(&'a self) -> Vec<&'a AstNode<'a>> {
+        match self {
+            &AstNode::Block { ref statements } => statements.clone(),
+            &AstNode::If { consequent, alternate, .. } => {
+                let mut res = vec![consequent];
+                if alternate.is_some() { res.push(alternate.unwrap()); }
+                res
+            },
+            &AstNode::FnDef { body, .. } => vec![body],
+            &AstNode::OptionBlock { statement, .. } => vec![statement],
+            &AstNode::Return {..} => vec![],
+            &AstNode::Expression {..} => vec![],
+            &AstNode::VarDefinition {..} => vec![],
+            &AstNode::StructDefinition {..} => vec![]
+        }
+    }
+}
+
+impl<'a> Children<'a, Expression<'a>> for AstNode<'a> {
+    fn children(&'a self) -> Vec<&'a Expression<'a>> {
+        match self {
+            &AstNode::Block { .. } => vec![],
+            &AstNode::If { condition, .. } => vec![condition],
+            &AstNode::FnDef { .. } => vec![],
+            &AstNode::OptionBlock { .. } => vec![], // TODO
+            &AstNode::Return { expression } => 
+                if expression.is_some() { vec![expression.unwrap()] }
+                else { vec![] },
+            &AstNode::Expression { expression } => vec![expression],
+            &AstNode::VarDefinition { initializer, .. } =>
+                if initializer.is_some() { vec![initializer.unwrap()] }
+                else { vec![] },
+            &AstNode::StructDefinition {..} => vec![]
+        }
+    }
+}
+
+impl<'a> Children<'a, Expression<'a>> for Expression<'a> {
+    fn children(&'a self) -> Vec<&'a Expression<'a>> {
+        match self {
+            &Expression::Integer {..} => vec![],
+            &Expression::Identifier {..} => vec![],
+            &Expression::Call { callee, ref args, .. } => {
+                let mut result: Vec<&'a Expression<'a>> = args.clone()
+                    .into_iter()
+                    .map(|a| { a.value })
+                    .collect();
+                result.push(callee);
+                result
+            },
+            &Expression::Dot { expression, .. } => vec![expression],
+            &Expression::Subscript { array, index, .. } => vec![array, index],
+            &Expression::UnaryOp { expression, .. } => vec![expression],
+            &Expression::BinaryOp { a, b, .. } => vec![a, b],
+            &Expression::Cast { expression, .. } => vec![expression]
+        }
+    }
 }

dc06d5e1c4685de3e49d6916627a864a7f1adbe0
Ryan Russell <russellsprouts2@gmail.com>
2018-05-13 16:59:21 -0700

    Fixed problem with multiple postfix expressions -- call, array index, property access

--- main.rs
+++ main.rs
@@ -11,30 +11,13 @@ use std::io::prelude::*;
 use std::vec::Vec;
 use std::cell::Cell;
 use typed_arena::Arena;
+use std::iter::FromIterator;
 
-use im::HashMap;
+use im::HashMap as ImmHashMap;
+use std::collections::HashMap;
 
 mod grammar {
     include!(concat!(env!("OUT_DIR"), "/grammar.rs"));
-}
-
-#[derive(Debug)]
-pub struct SymbolTable<'a, T: 'a> {
-    tables: HashMap<u64, HashMap<String, &'a T>>,
-    id: u64
-}
-
-impl<'a, T> SymbolTable<'a, T> {
-    fn new() -> SymbolTable<'a, T> {
-        SymbolTable {
-            tables: HashMap::new(),
-            id: 0
-        }
-    }
-
-    fn add(m: HashMap<String, T>) {
-
-    }
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -52,6 +35,81 @@ pub struct Expression2<'a> {
     loc: usize,
     id: u64,
     is: Expression2Type<'a>
+}
+
+impl<'a> Expression2<'a> {
+    fn prune<'b>(&self, ast: &'b Ast<'b>) -> &'b Expression2<'b> {
+        use Expression2Type::*;
+        ast.create(&Expression2 {
+            loc: self.loc,
+            id: ast.id(),
+            is: match self.is {
+                Integer(i) => Integer(i),
+                FixedPoint { value, scale } => FixedPoint { value, scale },
+                Identifier(s) => Identifier(ast.create(s)),
+                ArrayAccess { array, index } => ArrayAccess { array: array.prune(ast), index: index.prune(ast) },
+                Dot { expression, property } => Dot { expression: expression.prune(ast), property: ast.create(property) },
+                Call { callee, args } => Call {
+                    callee: callee.prune(ast),
+                    args: ast.create(&Vec::from_iter(args.into_iter().map(|&a| {
+                        (a.0.map(|s| { ast.create(s) }), a.1.prune(ast))
+                    })))
+                },
+                UnaryOp { expression, op } => UnaryOp { expression: expression.prune(ast), op },
+                BinaryOp { a, op, b } => BinaryOp { a: a.prune(ast), op, b: b.prune(ast) },
+                Cast { ex, new_type } => Cast {
+                    ex: ex.prune(ast),
+                    new_type: new_type.prune(ast)
+                },
+                StructInitialization { struct_type, args } => StructInitialization {
+                    struct_type: struct_type.prune(ast),
+                    args: ast.create(&Vec::from_iter(args.into_iter().map(|a| {
+                        (a.0.map(|s| { ast.create(s) }), a.1.prune(ast))
+                    })))
+                },
+                If { condition, consequent, alternate } => If {
+                    condition: condition.prune(ast),
+                    consequent: consequent.prune(ast),
+                    alternate: alternate.map(|a| { a.prune(ast) })
+                },
+                For { var, iter, body } => For { var: ast.create(var), iter: iter.prune(ast), body: body.prune(ast) },
+                Block { body, semi_terminated } => Block {
+                    body: ast.create(&Vec::from_iter(body.into_iter().map(|s| {
+                        s.prune(ast)
+                    }))),
+                    semi_terminated
+                },
+                OptionBlock { options, body } => OptionBlock { options, body: body.prune(ast) },
+                LetWithInitializer { var, declared_type, init } => LetWithInitializer {
+                    var: ast.create(var),
+                    declared_type: declared_type.map(|d| d.prune(ast)),
+                    init: init.prune(ast)
+                },
+                Let { var, declared_type } => Let { var: ast.create(var), declared_type: declared_type.prune(ast) },
+                Function { name, args, return_type, body } => Function {
+                    name: name.map(|n| { ast.create(n) }),
+                    args: ast.create(&Vec::from_iter(args.into_iter().map(|a| {
+                        (ast.create(a.0), a.1.prune(ast))
+                    }))),
+                    return_type: return_type.prune(ast),
+                    body: body.prune(ast)
+                },
+                StructDefinition { name, fields } => StructDefinition {
+                    name: ast.create(name),
+                    fields: ast.create(&Vec::from_iter(fields.into_iter().map(|f| {
+                        (ast.create(f.0), f.1.prune(ast))
+                    })))
+                },
+                Return { value } => Return { value: value.prune(ast) }
+            }   
+        })
+    }
+}
+
+pub enum ExpressionTail<'a> {
+    Dot { property: &'a str, loc: usize },
+    ArrayAccess { index: &'a Expression2<'a>, loc: usize },
+    Call { args: &'a [(Option<&'a str>, &'a Expression2<'a>)], loc: usize },
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -139,6 +197,15 @@ pub struct TypeArg<'a> {
     name : Option<&'a str>
 }
 
+impl<'a> TypeArg<'a> {
+    fn prune<'b>(&self, ast: &'b Ast<'b>) -> TypeArg<'b> {
+        TypeArg {
+            value: self.value.prune(ast),
+            name: self.name.map(|n| { ast.create(n) })
+        }
+    }
+}
+
 #[derive(Debug, Copy, Clone)]
 pub enum Type<'a> {
     Identifier(&'a str),
@@ -147,6 +214,24 @@ pub enum Type<'a> {
         args: &'a [TypeArg<'a>]
     },
     AnonymousStruct(&'a [(&'a str, Type<'a>)])
+}
+
+impl<'a> Type<'a> {
+    fn prune<'b>(&self, ast: &'b Ast<'b>) -> Type<'b> {
+        use Type::*;
+        match self {
+            &Identifier(s) => Identifier(ast.create(s)),
+            &GenericType { name, args } => GenericType {
+                name: ast.create(name),
+                args: ast.create(&Vec::from_iter(args.into_iter().map(|a| {
+                    a.prune(ast)
+                })))
+            },
+            &AnonymousStruct(fields) => AnonymousStruct(ast.create(&Vec::from_iter(fields.into_iter().map(|f| {
+                (ast.create(f.0), f.1.prune(ast))
+            }))))
+        }
+    }
 }
 
 pub struct Ast<'a> {
@@ -174,6 +259,10 @@ impl<'a> Ast<'a> {
             field_arr_factory: Arena::new(),
             last_id: Cell::new(0)
         }
+    }
+
+    fn size(&self) -> u64 {
+        self.last_id.get()
     }
 
     fn parse(ast: &'a Ast<'a>, str: &String) -> Expression2<'a> {
@@ -235,13 +324,26 @@ fn main() {
 
     println!("With text:\n{}", contents);
 
-    let ast = Ast::new();
-    let parsed = Ast::parse(&ast, &contents);
+    let mut parsed: &Expression2;
+    let pruned_ast = Ast::new();
+
+    {
+        let ast = Ast::new();
+        let original_parse = Ast::parse(&ast, &contents);
+        parsed = original_parse.prune(&pruned_ast);
+    }
+
     println!("Parsed as\n{:#?}", parsed);
+    
+    println!("Parsed {:#?}", Children::<Expression2>::children(parsed));
 
-    println!("Parsed {:#?}", Children::<Expression2>::children(&parsed));
+    let mut symbols = HashMap::new();
+    resolve_symbols(&parsed, &ImmHashMap::new(), &mut symbols);
 
-    resolve_symbols(&parsed, &HashMap::new());
+    println!("{:#?}", symbols);
+
+    //let mut types = HashMap::new();
+    //check_types(&parsed, &mut types);
 }
 
 trait Children<'a, T> {
@@ -291,7 +393,9 @@ impl<'a> Children<'a, Expression2<'a>> for Expression2<'a> {
     }
 }
 
-fn resolve_symbols<'a>(expr: &'a Expression2<'a>, map: &HashMap<String, Expression2<'a>>) {
+fn resolve_symbols<'a>(expr: &'a Expression2<'a>,
+    map: &ImmHashMap<String, Expression2<'a>>,
+    output: &mut HashMap<u64, Expression2<'a>>) {
     use Expression2Type::*;
 
     let mut new_map = map.clone();
@@ -302,7 +406,7 @@ fn resolve_symbols<'a>(expr: &'a Expression2<'a>, map: &HashMap<String, Expressi
             match map.get(&name.to_string()) {
                 None => panic!("Unresolved identifier! {}", name),
                 Some(exp) => {
-                    println!("RESOLVED {} as {:?}", name, exp);
+                    output.insert(expr.id, *exp);
                 }
             }
         },
@@ -312,31 +416,44 @@ fn resolve_symbols<'a>(expr: &'a Expression2<'a>, map: &HashMap<String, Expressi
                 new_map = new_map.insert(name.to_string(), expr)
             }
         },
-        Block { body, .. } => {
+        Block { .. } => {
             for child in expr.children() {
                 // These expressions add to the scope that encloses them.
                 match child.is {
                     Let { var, .. } => new_map = new_map.insert(var.to_string(), child),
                     LetWithInitializer { var, .. } => new_map = new_map.insert(var.to_string(), child),
-                    Function { name, .. } => if (name.is_some()) {
+                    Function { name, .. } => if name.is_some() {
                         new_map = new_map.insert(name.unwrap().to_string(), child)
                     },
                     _ => ()
                 }
-                resolve_symbols(child, &new_map);
+                resolve_symbols(child, &new_map, output);
             }
             return;
         },
         _ => ()
     }
     for child in expr.children() {
-        resolve_symbols(child, &new_map);
+        resolve_symbols(child, &new_map, output);
     }
 }
 
 trait PrintAst {
     fn print_pretty(&self, indent: String) -> String;
 }
+
+/*fn check_types<'a>(expr: &'a Expression2<'a>, output: &mut HashMap<u64, Type<'a>>) {
+    for child in expr.children() {
+        check_types(child, output);
+    }
+
+    use Expression2Type::*;
+    switch expr.is {
+        Integer(n) => output.insert(expr.id, 0),
+        FixedPoint { value, scale } => output.insert(expr.id, 0),
+
+    }
+}*/
 
 /*fn resolve_symbols_expr<'a>(expr: &'a Expression<'a>, map: &HashMap<String, AstNode<'a>>) {
     match expr {
